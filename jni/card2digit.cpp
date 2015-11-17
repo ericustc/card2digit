8e5f2efca44ddfca0a0c686efd6a5db6f0def0f1
cameoh <343473@gmail.com>
2015-11-17 22:54:30 +0800

    Discard the template matching code

--- jni/card2digit.cpp
+++ jni/card2digit.cpp
@@ -84,110 +84,12 @@ JNIEXPORT jstring JNICALL Java_com_example_card2digit_CameraPreview_ocr(
 		++step;
 	}
 
-	for (int i = 0; i < height; ++i) {
-		string s;
-		for (int j = 0; j < 50; ++j) {
-			s += binaryMat.at(j, i) ? "0" : "-";
-		}
-		__android_log_print(ANDROID_LOG_VERBOSE, "xxx", "%s", s.c_str());
-	}
-
 	env->ReleaseByteArrayElements(data, pixel, JNI_ABORT);
 	return env->NewStringUTF(res);
 }
 
 char recognize(mat<bool> &pixel, int l, int r, int t, int b) {
-	static unsigned short glyph[11][13] = { { 0x3C, 0x7E, 0xC3, 0xC3, 0xC3,
-			0xC3, 0xC3, 0xC3, 0xC3, 0xC3, 0xC3, 0x7E, 0x3C }, // 0
-
-			{ 0x0C, 0x3C, 0x7C, 0xEC, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C,
-					0x0C, 0x0C }, // 1
-
-			{ 0x3C, 0xFE, 0x43, 0x03, 0x03, 0x06, 0x0E, 0x38, 0x70, 0x60, 0xC0,
-					0xFF, 0xFF }, // 2
-
-			{ 0xFF, 0xFF, 0x06, 0x0C, 0x18, 0x3C, 0x0E, 0x03, 0x03, 0x03, 0x07,
-					0xFE, 0xFC }, // 3
-
-			{ 0x08, 0x18, 0x18, 0x30, 0x30, 0x64, 0x66, 0xC6, 0xFF, 0xFF, 0x06,
-					0x06, 0x06 }, // 4
-
-			{ 0xFE, 0xFE, 0xC0, 0xC0, 0xF0, 0xFC, 0x0C, 0x06, 0x06, 0x06, 0x0C,
-					0xF8, 0xF0 }, // 5
-
-			{ 0x04, 0x0C, 0x18, 0x30, 0x70, 0x7C, 0xFE, 0xC3, 0xC3, 0xC3, 0xC3,
-					0x7E, 0x3C }, // 6
-
-			{ 0xFF, 0xFF, 0x03, 0x03, 0x06, 0x0C, 0x18, 0x18, 0x30, 0x30, 0x30,
-					0x30, 0x30 }, // 7
-
-			{ 0x1C00, 0x7E00, 0x6300, 0x6300, 0x7600, 0x3C00, 0x3E00, 0x6300,
-					0xC180, 0xC180, 0xC380, 0x7F00, 0x3E00 }, // 8
-
-			{ 0x3C, 0x7E, 0xE7, 0xC3, 0xC3, 0xC3, 0xE3, 0x7E, 0x3E, 0x0C, 0x0C,
-					0x18, 0x30 }, // 9
-
-			{ 0xC3, 0x43, 0x66, 0x34, 0x3C, 0x18, 0x18, 0x3C, 0x26, 0x66, 0xC3,
-					0xC3 } // X
-
-	};
-
-	float ratio = (b - t) / 13.0f;
-
-	FILE * pFile = fopen ("/sdcard/training.data","a");
-	if (pFile!=NULL) {
-	    fprintf(pFile, "%d %d\n", r - l, b - t);
-	    for (int i = t; i < b; ++i) {
-	    	string s;
-	    	for (int j = l; j < r; ++j) {
-	    		s += pixel.at(j, i) ? "+" : "-";
-	    	}
-	    	fprintf(pFile, "%s\n", s.c_str());
-	    }
-	    fclose(pFile);
-	}
-
-	float grade = 0.0f;
-	int max = -1;
-	__android_log_print(ANDROID_LOG_VERBOSE, "xxx", "%d, %d, %d, %d, %f", l, r,
-			t, b, ratio);
-	for (int i = 0; i < 11; ++i) {
-		float g = compare(glyph[i], pixel, l, r, t, b, ratio);
-		__android_log_print(ANDROID_LOG_VERBOSE, "xxx", "%d, grade: %f", i, g);
-		if (g > grade) {
-			grade = g;
-			max = i;
-		}
-	}
-	if (max == 1 || max == 7) {
-		int count = 0;
-		for (int i = l; i < r; ++i) {
-			if (pixel.at(i, t)) {
-				++count;
-			}
-		}
-		return count / (float)(r - l) < 0.8f ? '1' : '7';
-	}
-	return max == 10 ? 'X' : '0' + max;
-}
-
-float compare(unsigned short glyph[], mat<bool> &pixel, int l, int r, int t, int b,
-		float ratio) {
-	int shift = glyph[0] > 0xFF ? 16 : 8;
-	int hit = 0;
-	for (int i = t; i < b; ++i) {
-		for (int j = l; j < r; ++j) {
-			int x = (int) ((j - l) / ratio);
-			bool flag =
-					x < shift
-							&& (glyph[(int) ((i - t) / ratio)]
-									& (1 << (shift - 1 - x)));
-			if (!(pixel.at(j, i) ^ flag)) {
-				++hit;
-			}
-		}
-	}
-	return hit / ((float) (r - l) * (b - t));
+	return '0';
 }
 
 int otsu(int histogram[], int total) {
